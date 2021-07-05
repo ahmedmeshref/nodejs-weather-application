c96f219d0daddc9f4b7a1d8269669e8300f56821
ahmedmeshref <a.meshref@alustudent.com>
2021-07-05 16:22:29 +0200

    Add files

--- config.js
+++
@@ -1,8 +0,0 @@
-const constants = {
-    openWeatherMap: {
-        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "d049b30b5cda0628bacfcde57b425575"
-    }
-}
-
-module.exports = constants;
